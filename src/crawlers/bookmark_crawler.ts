34552ae201c2c057081eb06c7855faa7f5a43659
non_hana <1209220829@qq.com>
2025-03-03 23:53:51 +0800

    feat: complete BookmarkCrawler

--- src/crawlers/bookmark_crawler.ts
+++ src/crawlers/bookmark_crawler.ts
@@ -2,12 +2,14 @@ import type { Agents } from 'got'
 import got from 'got'
 import { HttpProxyAgent, HttpsProxyAgent } from 'hpagent'
 import { StatusCodes } from 'http-status-codes'
+import pLimit from 'p-limit'
 import { Collector } from '~/collector/collector.ts'
 import { collect } from '~/collector/collector_unit.ts'
 import { selectBookmark } from '~/collector/selectors.ts'
 import { debug_config, download_config, network_config, user_config } from '~/configs/index.ts'
 import { Downloader } from '~/downloader/downloader.ts'
 import { assertError, assertWarn, printInfo } from '~/utils/logMessage.ts'
+import { sleep } from '~/utils/sleep.ts'
 
 interface IBookmarkCrawler {
   imageNum: number
@@ -49,21 +51,62 @@ export class BookmarkCrawler implements IBookmarkCrawler {
             ? { https: new HttpsProxyAgent({ proxy: network_config.proxy.https }) }
             : undefined
 
-        const response = await got(
-          url,
-          {
-            headers,
-            agent,
-            timeout: { connect: download_config.timeout },
-          },
-        )
+        const response = await got(url, { headers, agent, timeout: { connect: download_config.timeout } })
 
         if (response.statusCode === StatusCodes.OK) {
+          const body = JSON.parse(response.body)
+          const totalCount = body.body.public[0].cnt
+          this.imageNum = Math.min(this.imageNum, Number.parseInt(totalCount))
+          printInfo(`Select ${this.imageNum}/${totalCount} artworks`)
+          printInfo('========== Bookmark crawler end ==========')
+          return
         }
       }
       catch (error) {
-
+        assertWarn(!debug_config.show_error, `Failed to request count: ${error}`)
+        assertWarn(!debug_config.show_error, `Retry ${i + 1} times`)
+        await sleep(download_config.fail_delay)
       }
     }
+    assertWarn(false, 'Please check your cookie configuration')
+    assertError(false, '========== Fail to get bookmark count ==========')
+  }
+
+  async collect(perJsonWorkCount: number = 48) {
+    const pageCount = Math.ceil((this.imageNum - 1) / perJsonWorkCount)
+    printInfo(`========== Start collecting ${this.userId}'s bookmarks ==========`)
+
+    const urls = new Set<string>()
+    for (let i = 0; i < pageCount; i++) {
+      const url = `${this.userUrl}/bookmarks?tag=&offset=${i * perJsonWorkCount}&limit=${perJsonWorkCount}&rest=show&lang=zh`
+      urls.add(url)
+      const additionalHeaders = { COOKIE: user_config.cookie }
+
+      const limit = pLimit(download_config.num_threads)
+      const tasks = Array.from(urls).map(url =>
+        limit(async () => {
+          try {
+            const imageIds = await collect(url, selectBookmark, additionalHeaders)
+            if (imageIds) {
+              this.collector.add(imageIds)
+            }
+          }
+          catch (error) {
+            assertWarn(false, `Failed to collect bookmarks: ${error}`)
+          }
+        }),
+      )
+      await Promise.all(tasks)
+    }
+
+    printInfo(`========== End collecting ${this.userId}'s bookmarks ==========`)
+    printInfo(`Total ${this.collector.idGroup.size} bookmarks collected`)
+  }
+
+  async run() {
+    await this._requestCount()
+    await this.collect()
+    await this.collector.collect()
+    return await this.downloader.download()
   }
 }
