c87306002ebc4a42c5c6baec4034a5173812ffa1
non_hana <1209220829@qq.com>
2025-05-04 16:21:12 +0800

    feat: 完善书签下载功能

--- src/configs/modules/network_config.ts
+++ src/configs/modules/network_config.ts
@@ -25,7 +25,7 @@ type HeaderConfig = Record<string, string>
 const DEFAULT_NETWORK_CONFIG = {
   proxy: {},
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
   },
 }
 
--- src/crawlers/bookmark_crawler.ts
+++ src/crawlers/bookmark_crawler.ts
@@ -1,15 +1,11 @@
-import { error } from 'node:console'
-import { StatusCodes } from 'http-status-codes'
 import pLimit from 'p-limit'
-import { client } from '~/client/index.ts'
 import { Collector } from '~/collector/collector.ts'
 import { collect } from '~/collector/collector_unit.ts'
 import { selectBookmark } from '~/collector/selectors.ts'
-import { debug_config, download_config, network_config, user_config } from '~/configs/index.ts'
+import { download_config, user_config } from '~/configs/index.ts'
 import { Downloader } from '~/downloader/downloader.ts'
 import { handleError } from '~/utils/handleError.ts'
-import { printInfo, printWarn } from '~/utils/logMessage.ts'
-import { sleep } from '~/utils/sleep.ts'
+import { printInfo } from '~/utils/logMessage.ts'
 
 interface IBookmarkCrawler {
   imageNum: number
@@ -17,7 +13,6 @@ interface IBookmarkCrawler {
   userUrl: string
   downloader: Downloader
   collector: Collector
-  _requestCount: () => Promise<void>
   collect: (perJsonWorkCount: number) => Promise<void>
   run: () => Promise<string[] | number>
 }
@@ -42,42 +37,19 @@ export class BookmarkCrawler implements IBookmarkCrawler {
     this.collector = new Collector(this.downloader)
   }
 
-  async _requestCount() {
-    const url = `${this.userUrl}/bookmark/tags?lang=zh`
-    printInfo('========== Bookmark crawler start ==========')
-
-    const headers = { ...network_config.headers, Cookie: encodeURIComponent(user_config.cookie) }
-
-    for (let i = 0; i < download_config.retry_times; i++) {
-      try {
-        const response = await client(url, { headers, agent: network_config.agent, timeout: { connect: download_config.timeout } })
-
-        if (response.statusCode === StatusCodes.OK) {
-          const body = typeof response.body === 'object' ? response.body : JSON.parse(response.body as string)
-          const totalCount = body.body.public[0].cnt
-          this.imageNum = Math.min(this.imageNum, Number.parseInt(totalCount))
-          printInfo(`Select ${this.imageNum}/${totalCount} artworks`)
-          printInfo('========== Bookmark crawler end ==========')
-          return
-        }
-      }
-      catch (error) {
-        printWarn(debug_config.show_error, `Failed to request count: ${error}`)
-        printWarn(debug_config.show_error, `Retry ${i + 1} times`)
-        await sleep(download_config.fail_delay)
-      }
-    }
-    handleError(error, { exit: false, message: 'Please check your cookie configuration' })
-    handleError(error, { exit: false, message: '========== Fail to get bookmark count ==========' })
-  }
-
   async collect(perJsonWorkCount: number = 48) {
-    const pageCount = Math.ceil((this.imageNum - 1) / perJsonWorkCount)
+    const pageCount = Math.ceil(this.imageNum / perJsonWorkCount)
     printInfo(`========== Start collecting ${this.userId}'s bookmarks ==========`)
 
     const urls = new Set<string>()
     for (let i = 0; i < pageCount; i++) {
-      const url = `${this.userUrl}/bookmarks?tag=&offset=${i * perJsonWorkCount}&limit=${perJsonWorkCount}&rest=show&lang=zh`
+      const url = `${
+        this.userUrl
+      }/bookmarks?tag=&offset=${
+        i * perJsonWorkCount
+      }&limit=${
+        Math.min(perJsonWorkCount, this.imageNum - i * perJsonWorkCount)
+      }&rest=show&lang=zh`
       urls.add(url)
       const additionalHeaders = { Cookie: user_config.cookie }
 
@@ -103,7 +75,6 @@ export class BookmarkCrawler implements IBookmarkCrawler {
   }
 
   async run() {
-    await this._requestCount()
     await this.collect()
     await this.collector.collect()
     return await this.downloader.download()
